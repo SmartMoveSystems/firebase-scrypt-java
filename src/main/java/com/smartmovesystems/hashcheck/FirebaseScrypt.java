700ee4b34096658547db4ed71b0fe55858a6a9f7
Yann <yann.frizenschaf@smartmovesystems.com>
2020-09-09 15:00:16 +0930

    Add alternate encryption method using OpenSSL implementation

--- src/main/java/com/smartmovesystems/hashcheck/FirebaseScrypt.java
+++ src/main/java/com/smartmovesystems/hashcheck/FirebaseScrypt.java
@@ -13,9 +13,11 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.Key;
+import java.util.Arrays;
 
 /**
  * Java adaptation of https://github.com/firebase/scrypt
+ * Attempts using Java's built-in AES as well as an implementation of OpenSSL's AES
  */
 public class FirebaseScrypt {
 
@@ -41,7 +43,7 @@ public class FirebaseScrypt {
     /**
      * Check if the password hashes to the known ciphertext
      * @param passwd the user's password
-     * @param knownCipher the known password hash
+     * @param knownCipherText the known password hash
      * @param salt the salt
      * @param saltSep the salt separator
      * @param signer base64 signer key from firebase
@@ -51,21 +53,55 @@ public class FirebaseScrypt {
      * @throws UnsupportedEncodingException
      * @throws GeneralSecurityException
      */
-    public static boolean check(String passwd, String knownCipher, String salt, String saltSep, String signer, int rounds, int memcost) throws UnsupportedEncodingException, GeneralSecurityException {
+    public static boolean checkWithOpenSSl(String passwd, String knownCipherText, String salt, String saltSep, String signer, int rounds, int memcost) throws UnsupportedEncodingException, GeneralSecurityException {
         // hashing password
         byte[] hashedBytes = hashWithSalt(passwd, salt, saltSep, rounds, memcost);
 
         System.out.println("derivedKey:" + new String(Base64.encodeBase64(hashedBytes), CHARSET));
 
         // encrypting with aes
-        byte[] cipherBytes = encrypt(signer.getBytes(CHARSET), hashedBytes);
-//        byte[] cipherBytes = encryptOpenSSL(new String(Base64.decodeBase64(signer)), new String(hashedBytes));
+        byte[] signerBytes = Base64.decodeBase64(signer.getBytes(CHARSET));
+        byte[] cipherTextBytes = encryptOpenSSL(signerBytes, bytesToChars(hashedBytes));
 
-        String cipherString = new String(Base64.encodeBase64(cipherBytes), CHARSET);
+        byte[] knownCipherBytes = Base64.decodeBase64(knownCipherText.getBytes(CHARSET));
 
-        System.out.println("cipherString:" + cipherString);
+        String cipherTextString = new String(Base64.encodeBase64(cipherTextBytes), CHARSET);
 
-        return knownCipher.equals(cipherString);
+        System.out.println("cipherTextString:" + cipherTextString);
+
+        return Arrays.equals(knownCipherBytes, cipherTextBytes);
+    }
+
+    /**
+     * Check if the password hashes to the known ciphertext
+     * @param passwd the user's password
+     * @param knownCipherText the known password hash after encryption
+     * @param salt the salt
+     * @param saltSep the salt separator
+     * @param signer base64 signer key from firebase
+     * @param rounds rounds scrypt parameter
+     * @param memcost memcost scrypt parameter
+     * @return
+     * @throws UnsupportedEncodingException
+     * @throws GeneralSecurityException
+     */
+    public static boolean check(String passwd, String knownCipherText, String salt, String saltSep, String signer, int rounds, int memcost) throws UnsupportedEncodingException, GeneralSecurityException {
+        // hashing password
+        byte[] hashedBytes = hashWithSalt(passwd, salt, saltSep, rounds, memcost);
+
+        System.out.println("derivedKey:" + new String(Base64.encodeBase64(hashedBytes), CHARSET));
+
+        // encrypting with aes
+        byte[] signerBytes = Base64.decodeBase64(signer.getBytes(CHARSET));
+        byte[] cipherTextBytes = encrypt(signerBytes, hashedBytes);
+
+        byte[] knownCipherBytes = Base64.decodeBase64(knownCipherText.getBytes(CHARSET));
+
+        String cipherTextString = new String(Base64.encodeBase64(cipherTextBytes), CHARSET);
+
+        System.out.println("cipherTextString:" + cipherTextString);
+
+        return Arrays.equals(knownCipherBytes, cipherTextBytes);
     }
 
     private static Key generateKeyFromString(final byte[] keyVal) {
@@ -75,7 +111,7 @@ public class FirebaseScrypt {
     public static byte[] encrypt(final byte[] signer, final byte[] derivedKey) {
         try {
             final Key key = generateKeyFromString(derivedKey);
-            final byte[] nonce = ByteBuffer.allocate(8).putLong(0).array();
+            final byte[] nonce = ByteBuffer.allocate(12).putLong(0).array();
             byte[] iv = new byte[16];
             System.arraycopy(nonce, 0, iv, 0, nonce.length);
             IvParameterSpec ivSpec = new IvParameterSpec(iv);
@@ -102,15 +138,31 @@ public class FirebaseScrypt {
             System.err.print(ex);
             return null;
         }
-
     }
 
-    public static byte[] encryptOpenSSL(String signer, final String derivedKey) {
+    public static byte[] encryptOpenSSL(byte[] plaintext, char[] pwd) {
         try {
-            return OpenSSL.encrypt("aes-256", derivedKey.toCharArray(), signer.getBytes(CHARSET), true, false);
+            return OpenSSL.encrypt("aes-256-ctr", pwd, plaintext, false, false);
         } catch(Exception ex) {
             System.err.print(ex);
             return null;
         }
+    }
+
+    public static byte[] decryptOpenSSL(byte[] encrypted, final char[] pwd) {
+        try {
+            return OpenSSL.decrypt("aes-256-ctr", pwd, encrypted);
+        } catch(Exception ex) {
+            System.err.print(ex);
+            return null;
+        }
+    }
+
+    public static char[] bytesToChars(byte[] byteBuf) {
+        char[] charBuf = new char[byteBuf.length];
+        for (int i = 0; i < byteBuf.length; i++) {
+            charBuf[i] = (char) (byteBuf[i] & 0xff);
+        }
+        return charBuf;
     }
 }
