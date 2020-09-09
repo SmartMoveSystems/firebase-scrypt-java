81b70484b6dd4c9f023948b2ae68a46f3840c5b7
Yann <yann.frizenschaf@smartmovesystems.com>
2020-09-09 12:31:40 +0930

    Explicitly specify Iv and nonce for AES CTR

--- src/main/java/com/smartmovesystems/hashcheck/FirebaseScrypt.java
+++ src/main/java/com/smartmovesystems/hashcheck/FirebaseScrypt.java
@@ -5,8 +5,10 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.ssl.OpenSSL;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
@@ -17,7 +19,7 @@ import java.security.Key;
  */
 public class FirebaseScrypt {
 
-    private static final Charset CHARSET = StandardCharsets.UTF_8;
+    private static final Charset CHARSET = StandardCharsets.US_ASCII;
 
     public static byte[] hashWithSalt(String passwd, String salt, String saltSep, int rounds, int memcost) throws GeneralSecurityException {
         int N = 1 << memcost;
@@ -52,12 +54,12 @@ public class FirebaseScrypt {
     public static boolean check(String passwd, String knownCipher, String salt, String saltSep, String signer, int rounds, int memcost) throws UnsupportedEncodingException, GeneralSecurityException {
         // hashing password
         byte[] hashedBytes = hashWithSalt(passwd, salt, saltSep, rounds, memcost);
-        String derivedKey = new String(Base64.encodeBase64(hashedBytes), CHARSET);
 
-        System.out.println("derivedKey:" + derivedKey);
+        System.out.println("derivedKey:" + new String(Base64.encodeBase64(hashedBytes), CHARSET));
 
         // encrypting with aes
-        byte[] cipherBytes = encrypt(signer, derivedKey);
+        byte[] cipherBytes = encrypt(signer.getBytes(CHARSET), hashedBytes);
+//        byte[] cipherBytes = encryptOpenSSL(new String(Base64.decodeBase64(signer)), new String(hashedBytes));
 
         String cipherString = new String(Base64.encodeBase64(cipherBytes), CHARSET);
 
@@ -66,44 +68,48 @@ public class FirebaseScrypt {
         return knownCipher.equals(cipherString);
     }
 
-    private static Key generateKeyFromString(final String secKey) {
-        final byte[] keyVal = Base64.decodeBase64(secKey.getBytes(CHARSET));
+    private static Key generateKeyFromString(final byte[] keyVal) {
         return new SecretKeySpec(keyVal, 0, 32, "AES");
     }
 
-
-    public static byte[] encrypt(String signer, final String derivedKey) {
+    public static byte[] encrypt(final byte[] signer, final byte[] derivedKey) {
         try {
             final Key key = generateKeyFromString(derivedKey);
+            final byte[] nonce = ByteBuffer.allocate(8).putLong(0).array();
+            byte[] iv = new byte[16];
+            System.arraycopy(nonce, 0, iv, 0, nonce.length);
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
             final Cipher c = Cipher.getInstance("AES/CTR/NoPadding");
-            c.init(Cipher.ENCRYPT_MODE, key);
-            return c.doFinal(signer.getBytes(CHARSET));
+            c.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+            return c.doFinal(signer);
         } catch(Exception ex) {
-            System.out.println(ex);
+            System.err.print(ex);
             return null;
         }
     }
 
-    public static byte[] decrypt(String signer, final String derivedKey) {
-
+    public static byte[] decrypt(final byte[] signer, final byte[] derivedKey) {
         try {
             final Key key = generateKeyFromString(derivedKey);
+            final byte[] nonce = ByteBuffer.allocate(8).putLong(0).array();
+            byte[] iv = new byte[16];
+            System.arraycopy(nonce, 0, iv, 0, nonce.length);
+            IvParameterSpec ivSpec = new IvParameterSpec(iv);
             final Cipher c = Cipher.getInstance("AES/CTR/NoPadding");
-            c.init(Cipher.DECRYPT_MODE, key);
-            return c.doFinal(signer.getBytes(CHARSET));
+            c.init(Cipher.DECRYPT_MODE, key, ivSpec);
+            return c.doFinal(signer);
         } catch(Exception ex) {
-            System.out.println("The Exception is=" + ex);
+            System.err.print(ex);
             return null;
         }
 
     }
 
     public static byte[] encryptOpenSSL(String signer, final String derivedKey) {
-
         try {
-            return OpenSSL.encrypt("aes-256", derivedKey.toCharArray(), signer.getBytes(CHARSET), false, false);
+            return OpenSSL.encrypt("aes-256", derivedKey.toCharArray(), signer.getBytes(CHARSET), true, false);
         } catch(Exception ex) {
-            System.out.println("The Exception is=" + ex);
+            System.err.print(ex);
             return null;
         }
     }
